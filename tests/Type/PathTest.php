389a54b4714b9c7e941b3cce6d0b872c359352b1
Nicolò Martini <nicolo@martini.io>
2016-04-05 01:04:51 +0100

    Path tests

---
+++ tests/Type/PathTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Nicolò Martini - <nicolo.martini@dxi.eu>
+ *
+ * Created on 05/04/2016, 14:43
+ * Copyright (C) DXI Ltd
+ */
+
+namespace NicMart\Generics\Type;
+
+
+class PathTest extends \PHPUnit_Framework_TestCase
+{
+
+}
