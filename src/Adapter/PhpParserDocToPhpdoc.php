475f7565e521375a64527e65f34ece894dbac590
Nicolò Martini <nicolo@martini.io>
2016-04-10 17:27:54 +0100

    PhpParser Doc to PHPDocumentor docblock converter class

---
+++ src/Adapter/PhpParserDocToPhpdoc.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * This file is part of php-generics
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Nicolò Martini <nicolo@martini.io>
+ */
+
+namespace NicMart\Generics\Adapter;
+
+
+use NicMart\Generics\Name\Context\NamespaceContext;
+use phpDocumentor\Reflection\DocBlock;
+use PhpParser\Comment\Doc;
+
+/**
+ * Class PhpParserDocToPhpdoc
+ * @package NicMart\Generics\Adapter
+ */
+class PhpParserDocToPhpdoc
+{
+    /**
+     * @param Doc $phpdoc
+     * @param NamespaceContext $namespaceContext
+     * @return DocBlock
+     * @throws \InvalidArgumentException
+     */
+    public function transform(Doc $phpdoc, NamespaceContext $namespaceContext)
+    {
+        $namespace = $namespaceContext->getNamespace()->toString();
+
+        $uses = array();
+        foreach ($namespaceContext->getUsesByAliases() as $alias => $use) {
+            $uses[$use->name()->toString()] = $use->alias()->toString();
+        }
+
+        return new DocBlock(
+            $phpdoc->getText(),
+            new DocBlock\Context(
+                $namespace,
+                $uses
+            )
+        );
+    }
+}
---
+++ tests/Unit/Adapter/PhpParserDocToPhpdocTest.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * This file is part of php-generics
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Nicolò Martini <nicolo@martini.io>
+ */
+
+namespace NicMart\Generics\Adapter;
+
+
+use NicMart\Generics\Name\Context\Namespace_;
+use NicMart\Generics\Name\Context\NamespaceContext;
+use NicMart\Generics\Name\Context\Use_;
+use phpDocumentor\Reflection\DocBlock;
+use PhpParser\Comment\Doc;
+
+class PhpParserDocToPhpdocTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     * @throws \InvalidArgumentException
+     */
+    public function it_transforms()
+    {
+        $transformer = new PhpParserDocToPhpdoc();
+        $doc = new Doc('
+            /**
+             * @param string $x Bla bla
+             */
+        ');
+
+        $nsContext = new NamespaceContext(
+            Namespace_::fromString("Ns1\\Ns2"),
+            array(
+                Use_::fromStrings("A", "B"),
+                Use_::fromStrings("C\\D", "E"),
+            )
+        );
+
+        $expected = new DocBlock(
+            $doc->getText(),
+            new DocBlock\Context(
+                "Ns1\\Ns2",
+                array(
+                    "A" => "B",
+                    "C\\D" => "E"
+                )
+            )
+        );
+
+        $this->assertEquals(
+            $expected,
+            $transformer->transform($doc, $nsContext)
+        );
+    }
+}
