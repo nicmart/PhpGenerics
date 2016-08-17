bc1154358c83b7537316072e0cf63bfb73cab5ea
nicmart <nicolo@martini.io>
2016-08-17 19:58:00 +0100

    TypeAnnotatorBlockFactory

--- src/Infrastructure/PhpDocumentor/TypeAnnotatorBlockFactory.php
+++ src/Infrastructure/PhpDocumentor/TypeAnnotatorBlockFactory.php
@@ -11,25 +11,47 @@
 namespace NicMart\Generics\Infrastructure\PhpDocumentor;
 
 
+use NicMart\Generics\Type\Parser\TypeParser;
 use phpDocumentor\Reflection\DocBlock;
+use phpDocumentor\Reflection\DocBlock\DescriptionFactory;
+use phpDocumentor\Reflection\DocBlock\StandardTagFactory;
 use phpDocumentor\Reflection\DocBlockFactory;
-use phpDocumentor\Reflection\DocBlockFactoryInterface;
-use phpDocumentor\Reflection\Location;
+use phpDocumentor\Reflection\FqsenResolver;
+use phpDocumentor\Reflection\TypeResolver;
 use phpDocumentor\Reflection\Types;
 
-class TypeAnnotatorBlockFactory implements DocBlockFactoryInterface
+/**
+ * Class TypeAnnotatorBlockFactory
+ * @package NicMart\Generics\Infrastructure\PhpDocumentor
+ */
+class TypeAnnotatorBlockFactory
 {
-    public static function createInstance(array $additionalTags = [])
-    {
-        // TODO: Implement createInstance() method.
-    }
-
-    public function create(
-        $docblock,
-        Types\Context $context = null,
-        Location $location = null
+    /**
+     * @param TypeParser $typeParser
+     * @param array $additionalTags
+     * @return DocBlockFactory
+     */
+    public static function createInstance(
+        TypeParser $typeParser,
+        array $additionalTags = []
     ) {
-        // TODO: Implement create() method.
-    }
+        $fqsenResolver = new FqsenResolver();
+        $tagFactory = new StandardTagFactory($fqsenResolver);
+        $descriptionFactory = new DescriptionFactory($tagFactory);
 
+        $tagFactory->addService($descriptionFactory);
+        $tagFactory->addService(new TypeResolver($fqsenResolver));
+
+        $tagAnnotatorFactory = new TypeAnnotatorTagFactory(
+            $tagFactory,
+            $typeParser
+        );
+
+        $docBlockFactory = new DocBlockFactory($descriptionFactory, $tagAnnotatorFactory);
+        foreach ($additionalTags as $tagName => $tagHandler) {
+            $docBlockFactory->registerTagHandler($tagName, $tagHandler);
+        }
+
+        return $docBlockFactory;
+    }
 }
